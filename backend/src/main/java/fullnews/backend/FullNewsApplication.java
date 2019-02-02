6309518a0508b4bc7d5d7dcc34b76be35e276759
pokemzok <pokemzok@mail.com>
2019-02-02 11:36:55 +0100

    News api integration

--- backend/src/main/java/fullnews/backend/FullNewsApplication.java
+++ backend/src/main/java/fullnews/backend/FullNewsApplication.java
@@ -2,8 +2,10 @@ package fullnews.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:news-api-key.properties", ignoreResourceNotFound = true)
 public class FullNewsApplication {
 
     public static void main(String[] args) {
--- backend/src/main/java/fullnews/backend/core/NewsService.java
+++ backend/src/main/java/fullnews/backend/core/NewsService.java
@@ -1,17 +1,33 @@
 package fullnews.backend.core;
 
 import fullnews.backend.api.NewsResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 
 @Service
 public class NewsService {
 
+    private String newsApiHeadlinesUrl;
+    private String newsApiKey;
+
+    public NewsService(
+            @Value("${newsapi.top-headlines.url}") String newsApiHeadlinesUrl,
+            @Value("${newsapi.key}") String newsApiKey
+    ) {
+        this.newsApiHeadlinesUrl = newsApiHeadlinesUrl;
+        this.newsApiKey = newsApiKey;
+    }
+
+    //FIXME TestNG] [ERROR] No test suite found. Nothing to run
     public NewsResponse getNews(String country, String category){
         var newsResponse = new NewsResponse(country, category, new ArrayList<>());
-        // FIXME integration with https://newsapi.org
+        RestTemplate restTemplate = new RestTemplate();
+
         // Think about next test (where should it be? Should test integration with news api)
+
         return newsResponse;
     }
 
