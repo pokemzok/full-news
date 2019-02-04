ddf2d7877f3f0cb2665205d5c49e2eb4b2f28ca5
pokemzok <pokemzok@mail.com>
2019-02-04 19:42:09 +0100

    article component

--- frontend/src/app/news/news.service.ts
+++ frontend/src/app/news/news.service.ts
@@ -3,17 +3,20 @@ import {HttpClient} from '@angular/common/http';
 import {GET_NEWS} from '../../environments/environment';
 import {Observable} from 'rxjs';
 import {News} from './news';
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class NewsService {
   constructor(private http: HttpClient) {
   }
 
-  getNews(category: string, country: string): Observable<News>  {
+  getNews(category: string, country: string): Observable<News> {
     return this.http.get(
       GET_NEWS
         .replace('REPLACE_COUNTRY', country)
         .replace('REPLACE_CATEGORY', category)
+    ).pipe(
+      map(response => response as News)
     );
   }
 }
