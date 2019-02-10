3224a05f506123513ee95997a3e6709efc1c5f6b
pokemzok <pokemzok@mail.com>
2019-02-10 19:49:48 +0100

    Swagger support

--- backend/src/main/java/fullnews/backend/FullNewsApplication.java
+++ backend/src/main/java/fullnews/backend/FullNewsApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @PropertySource(value = "classpath:news-api-key.properties")
 @EnableConfigurationProperties
+@EnableSwagger2
 public class FullNewsApplication {
 
     public static void main(String[] args) {
--- backend/src/main/java/fullnews/backend/web/NewsController.java
+++ backend/src/main/java/fullnews/backend/web/NewsController.java
@@ -2,6 +2,8 @@ package fullnews.backend.web;
 
 import fullnews.backend.api.NewsResponse;
 import fullnews.backend.core.NewsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/facade/")
 @Slf4j
 @RequiredArgsConstructor
+@Api
 public class NewsController {
 
     private final NewsService newsService;
 
+    @ApiOperation("Endpoint which serves as a gateway between frontend and News API")
     @GetMapping("/news/{country}/{category}")
     public NewsResponse news(@PathVariable String country, @PathVariable String category){
         log.info("Received news request for country {} and category {}",country, category);
